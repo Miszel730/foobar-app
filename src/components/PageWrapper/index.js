98752e0b45ae67ba1bfd05749a446ac7adba48de
michaliwaniec <mich6h94@stud.kea.dk>
2021-06-01 19:25:48 +0200

    big screens option blocked

--- src/components/PageWrapper/index.js
+++ src/components/PageWrapper/index.js
@@ -4,7 +4,7 @@ const PageWrapper = (props) => {
   return (
     <div className="pageWrapper">
       <img className="logo" src="/img/moon-bar-logo.svg" />
-
+      <div className="desktop-blur">The App is not ready for desktop...</div>
       {props.children}
       <footer className="footer">
         <p>All rights reserved - FooBar Space Industry 2074</p>
